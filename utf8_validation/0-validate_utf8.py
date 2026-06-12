daab81d8be439b0dee4e785dfe24ec57e7308a6a
Laurent Lacôte <laurent.lacote@private.com>
2026-06-12 11:42:27 +0200

    Py - Algs - UTF-8_Val - Feat: validation module

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -2,8 +2,66 @@
 """Single func module to validate a suite of numbers as valid UTF-8 string """
 
 
+def check_multibyte_header(byte) -> int:
+    """
+        Uses binary & comparison to check pattern matches header byte
+        by applying a "mask" which only keeps the relevant '1'.
+        Directly returns the number of "continuation bytes" expected.
+    """
+    if (byte & 0b10000000) == 0b00000000:  # 0xxxxxxx  → 1 byte char
+        return 0  # Will be understood as False by caller
+    elif (byte & 0b11100000) == 0b11000000:  # 110xxxxx  → 2 bytes char
+        return 1
+    elif (byte & 0b11110000) == 0b11100000:  # 1110xxxx  → 3 bytes char
+        return 2
+    elif (byte & 0b11111000) == 0b11110000:  # 11110xxx  → 4 bytes char
+        return 3
+    else:
+        return 0
+
+
+def is_continuation_byte(byte) -> bool:
+    """Check if byte matches the 10xxxxxx mask which marks 'continuation'"""
+    return (byte & 0b11000000) == 0b10000000        # 10xxxxxx
+
+
 def validUTF8(data: list) -> bool:
-    pass
+    """Validates that list of int can be interpreted as valid UTF8 string"""
+    if len(data) == 0:
+        return False
+
+    data_is_valid_UTF8_sequence = None
+    # Renamed from multibyte_counter which was less explicit
+    expected_continuation_bytes = 0
+    # Loop parsing potential UTF-8 sequence
+    for n in data:
+        # Reduce the int to its 8 lesser binary digits
+        # Using 255 in binary and the & binary operator
+        #   which only keeps the 1 present "both sides"
+        current_byte = n & 0b11111111  # Hexa would be 0xFF
+
+        # Determine the "nature" of current int
+        # If multibyte > 0 this MUST be a continuation byte
+        if expected_continuation_bytes > 0:
+            if is_continuation_byte(current_byte):
+                expected_continuation_bytes -= 1
+                continue
+            else:
+                data_is_valid_UTF8_sequence = False
+                break
+        # Multibyte counter is currently 0 so we can
+        #   directly affect it.
+        else:
+            expected_continuation_bytes = check_multibyte_header(current_byte)
+            if expected_continuation_bytes == 0:
+                if is_continuation_byte(current_byte):
+                    data_is_valid_UTF8_sequence = False
+                    break
+            else:
+                continue
+    else:
+        data_is_valid_UTF8_sequence = (expected_continuation_bytes == 0)
+    return data_is_valid_UTF8_sequence
 
 # === BUSINESS REQUIREMENTS ===
 # Return: True if data is a valid UTF-8 encoding, else return False
@@ -54,3 +112,15 @@ def validUTF8(data: list) -> bool:
 #   Return data_is_valid_UTF8_sequence
 # In v2 I could add the fact I store the "reduced byte"
 #   to recreate a printable UTF-8 string. :)
+# NOTE: comparing "as string" would be possible but far less performant
+#   because of conversion cost
+# bits = format(byte, '08b')  # ex: 195 → "11000011"
+#
+# if bits[0] == '0':           # 0xxxxxxx
+#     n_bytes = 1
+# elif bits[:3] == '110':      # 110xxxxx
+#     n_bytes = 2
+# elif bits[:4] == '1110':     # 1110xxxx
+#     n_bytes = 3
+# elif bits[:5] == '11110':    # 11110xxx
+#     n_bytes = 4
