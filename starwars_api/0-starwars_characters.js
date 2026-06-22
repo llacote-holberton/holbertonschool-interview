c8a8b07f5a5256ef526bb5d84b8cf0f3a25da380
Laurent Lacôte <laurent.lacote@private.com>
2026-06-22 10:40:20 +0200

    JS - Algs - SWA - Draft: trying to make a request
    Trying to apply the example provided in Request docs,
      just slightly adapted.

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,5 +1,21 @@
 
+// Grabbing the "request engine" and setting it as a constant.
+const request = require('request');
 
+my_test_request = request(
+    // First parameter is the url of the page we want to reach
+    'https://www.york.ac.uk/teaching/cws/wws/webpage1.html',
+    // Second parameter is an "anonymous function" which can do
+    //   different things depending on what was received in return
+    //   (valid answer or HTTP error code)
+    // WARNING: you can give whatever names but the order IS IMPOSED
+    //   as request() will always return these informations like this.
+    function (error, response, body ) 
+    {
+        console.log(error);
+        console.log(response.statusCode);
+    }
+)
 
 
 /* ========== BUSINESS GOAL ==========
