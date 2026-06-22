306a91956749726ab70f6a420cff020742df9071
Laurent Lacôte <laurent.lacote@private.com>
2026-06-22 19:45:38 +0200

    JS - Algs - SWA - Fix: codestyles
    - Fixed over-indentation overall.
    - Fixed missing spacing before parameters
        in function signatures overall.
    - Improved printMovieCharactersNames
      * replaced response.body with body as for
        the other request.

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -10,7 +10,7 @@ main();
  * Script's "orchestration function"
  *
  */
-function main() {
+function main () {
   if (!validArguments()) process.exit(1);
 
   const movieId = Number(process.argv[2]);
@@ -21,7 +21,7 @@ function main() {
  * Just ensures that the script was given valid arguments to work.
  * @returns {Boolean} valid
  */
-function validArguments() {
+function validArguments () {
   if (process.argv.length < 3) {
     const msgErrorArgMissing = 'Argument "movie id" (integer) is missing, cannot proceed!';
     console.error(msgErrorArgMissing);
@@ -42,48 +42,45 @@ function validArguments() {
  * @param {Number} movieId
  * @returns Array[Number]|Undefined
  */
-function printMovieCharactersNames(movieId) {
+function printMovieCharactersNames (movieId) {
   // @note: going for simplicity, setting the separating / statically in components strings.
   const apiRootUrl = 'https://swapi-api.hbtn.io/api/';
   const moviesEndpoint = 'films/';
   const movieRequestUrl = apiRootUrl + moviesEndpoint + movieId;
 
-  request( movieRequestUrl, function(error, response, body) {
-      if (error) {
-        console.error("Request couldn't be processed properly.");
-        return;
-      }
-
-      const responseHttpCode = Number(response.statusCode);
-
-      if (responseHttpCode !== 200) {
-        reportMovieRequestError(responseHttpCode);
-        return;
-      }
-
-      const charactersEndpoints = JSON.parse(response.body).characters;
-
-      // Using temporary variables to control when and how print names.
-      const charactersNames = [];
-      const requestsToMakeCount = charactersEndpoints.length;
-      let counter = 0;
-
-      charactersEndpoints.forEach((endpoint, index) => {
-          getCharacterName( endpoint, index, (charIdx, charName) => {
-              // Condition still useful to avoid printing "null" lines.
-              if (charName) charactersNames[charIdx] = charName;
-              counter++;
-              // Condition required to delay printing until all requests finished.
-              if (counter === requestsToMakeCount) charactersNames.forEach(name => console.log(name));
-            }
-          )
-        }
-      )
+  request(movieRequestUrl, function (error, response, body) {
+    if (error) {
+      console.error("Request couldn't be processed properly.");
+      return;
     }
-  );
+
+    const responseHttpCode = Number(response.statusCode);
+
+    if (responseHttpCode !== 200) {
+      reportMovieRequestError(responseHttpCode);
+      return;
+    }
+
+    const charactersEndpoints = JSON.parse(body).characters;
+
+    // Using temporary variables to control when and how print names.
+    const charactersNames = [];
+    const requestsToMakeCount = charactersEndpoints.length;
+    let counter = 0;
+
+    charactersEndpoints.forEach((endpoint, index) => {
+      getCharacterName(endpoint, index, (charIdx, charName) => {
+        // Condition still useful to avoid printing "null" lines.
+        if (charName) charactersNames[charIdx] = charName;
+        counter++;
+        // Condition required to delay printing until all requests finished.
+        if (counter === requestsToMakeCount) charactersNames.forEach(name => console.log(name));
+      });
+    });
+  });
 }
 
-function reportMovieRequestError(httpCode) {
+function reportMovieRequestError (httpCode) {
   if (httpCode === 404) console.log('Movie not found');
   else console.log('Some internal error occured on API side');
 }
@@ -94,15 +91,14 @@ function reportMovieRequestError(httpCode) {
  * @param {Function} characterNamesFillerCallback
  * @returns {String} characterName
  * @note: MUST be written "as a function with callback" because otherwise
- *   it would just immediately return "undefined" to the caller because
- *   getCharacterName called synchronously but making an asynchronous call within.
- *   ALSO WHY I must also give the 'index' as parameter (otherwise that info is "lost in time")
+ * it would just immediately return "undefined" to the caller because
+ * getCharacterName called synchronously but making an asynchronous call within.
+ * ALSO WHY I must also give the 'index' as parameter (otherwise that info is "lost in time")
  */
 function getCharacterName (characterEndpoint, index, characterNamesFillerCallback) {
   request(characterEndpoint, function (error, response, body) {
-      let characterName = null;
-      if (!error && response.statusCode === 200) { characterName = JSON.parse(body).name; }
-      characterNamesFillerCallback(index, characterName);
-    }
-  )
+    let characterName = null;
+    if (!error && response.statusCode === 200) { characterName = JSON.parse(body).name; }
+    characterNamesFillerCallback(index, characterName);
+  });
 }
