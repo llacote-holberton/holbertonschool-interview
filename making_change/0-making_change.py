8d162e0f77a32306a183494b96d1a93f67b29bb0
Laurent Lacôte <laurent.lacote@private.com>
2026-07-06 14:05:00 +0200

    Py - Algs - MkChgs - Clean: Pycodestyle

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -90,11 +90,9 @@ def makeChange(coins, total) -> int:
 # * If total cannot be met by any number of coins you have, return -1
 # * Coins is a list of the values of the coins in your possession.
 #   The value of a coin will always be an integer greater than 0
-# * You can assume you have an infinite number of 
+# * You can assume you have an infinite number of
 #      each denomination of coin in the list
 # * Your solution's runtime will be evaluated in this task
-
-
 
 # ========== BRAINSTORM ==========
 # So we need to find how to combine in the minimum of operations
@@ -111,41 +109,39 @@ def makeChange(coins, total) -> int:
 # Ex: target value 4, coins 1, 2, 4.
 # 1: 1 <= 1 so 1 coin of 1 to make 1.
 #
-# 2: Trying coin 1: we take the number of coins we used 
+# 2: Trying coin 1: we take the number of coins we used
 #     to get '1', we add one more coin of same value 1, we check
 #     if result is inferior or equal to target. Here, true.
 #    So "for target value 2", at this step the minimum combination found is 2.
 #    Then we try coin 2: it's <= '2', since it's the first try we just "add 1"
 #      is coin '2'* 1 count <= 2 ? YES
 #    Is that "coin count" is inferior to the previous combination found (1+1)
-#    We must a) affect that "new best minimum" as value 
+#    We must a) affect that "new best minimum" as value
 #      for the cell of index "current target value".
 #    Coin 4 is > to target value so ignored
 #
 # 3: Trying coin 1: 1 <= 3 True. If we "put one 1 on table" what's left ? 2
-#    Is there a combination allowing is to get "2" ? 
+#    Is there a combination allowing is to get "2" ?
 #    We check our array at index 2.
-#    Because we tried 2 before, and there is a value for it, 
+#    Because we tried 2 before, and there is a value for it,
 #      we read that value: 1 (one coin of value 2).
-#    We then check if that "amount of coins" is inferior to 
+#    We then check if that "amount of coins" is inferior to
 #    what is currently stored as "number of coins" for that target value.
 #    => How to get a "default" indicating that we didn't compute anything yet
 #    BUT can be checked with a comparison operator? None wouldn't work.
 #    => Simplest is to set a clear limit: the "total" amount is probably right.
 #       Or maybe total + 1 ? Something to clear once I develop.
-#    Back on track: is 1 + 1 inferior to target (4) ? YES. 
+#    Back on track: is 1 + 1 inferior to target (4) ? YES.
 #    We affect 2 as the value for array[3] -> "best combination found so far"
 #
 #    Trying coin 2: 2 <= 3 True. If we "put one 2 on table" what's left? 1".
 #    Do we have a combination of pieces to get value 1? Check array[1], value 1
-#    Is 1+1 strictly inferior (so better) to "previously best found combination"
+#    Is 1+1 strictly < (so better) to "previously best found combination"
 #    NO -> We don't do anything.
 #
 #    Trying coin 3: 3 <=3 True. If we "put one 3 on table" wha's left? 0".
 #    We fall on our feet since by design array[0] = 0.
 #    Is 0 + 1 < 2 (best solution found until then)? YES.
 #    => We affect 1 as the new best solution for array[3].
-#    AND if we want, we can also have an array indicating which is the 
+#    AND if we want, we can also have an array indicating which is the
 #      "best starting coin" for reaching that value (so like, choice[3] = 3).
-
-
